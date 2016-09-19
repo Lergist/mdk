f364744717fc5b7a397c3a00a4df262c51af6243
lergist <kursantdmp@gmail.com>
2016-09-19 13:23:10 +0500

    Привязал к бд форму авторизации

--- testirbas/testirbas/Form1.cs
+++ testirbas/testirbas/Form1.cs
@@ -12,9 +12,20 @@ namespace testirbas
 {
     public partial class Form1 : Form
     {
+        registred reg = new registred();
+        Form2 f2 = new Form2();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string prior = reg.login(textBox1.Text, textBox2.Text);
+            if(prior == "1")
+            {
+                f2.Show();
+            }
         }
     }
 }
--- testirbas/testirbas/registred.cs
+++ testirbas/testirbas/registred.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace testirbas
 {
     class registred
     {
-
+        public string login(string password, string login)
+        {
+            using (SqlConnection con = new SqlConnection(testirbas.Properties.Settings.Default.Setting))
+            {
+                string zap = @"select prioritet from users where login =" + login + " and password" + password + ";";
+                try
+                {
+                    SqlCommand com = new SqlCommand(zap, con);
+                    con.Open();
+                    return com.ExecuteScalar().ToString();
+                }
+                catch(Exception ex)
+                {
+                    return "0";
+                }
+            }
+        }
     }
 }
